2830f0c34679e079001ff44b318b3ed42f26535e
Fabio Mayer <mayer.fabio@outlook.com>
2024-05-23 01:35:09 +0200

    Adds regbit.hpp

---
+++ src/cpu/regbit.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cinttypes>
+
+template <unsigned bitno, unsigned nbits = 1, typename T = uint8_t>
+class RegBit {
+    T data;
+
+    enum { mask = (1u << nbits) - 1u };
+
+    template <typename T2>
+    RegBit& operator=(T2 val) {
+        data = (data & ~(mask << bitno)) | (val & mask) << bitno; 
+    }
+
+    operator unsigned() const { return (data >> bitno) & mask; }
+};
