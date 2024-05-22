d4867757e577628a9faed585c51ae88c9bbb8e41
Fabio Mayer <mayer.fabio@outlook.com>
2024-05-23 01:43:44 +0200

    Adds public and private areas of RegBit

--- src/cpu/regbit.hpp
+++ src/cpu/regbit.hpp
@@ -3,10 +3,11 @@
 
 template <unsigned bitno, unsigned nbits = 1, typename T = uint8_t>
 class RegBit {
+private:
     T data;
-
     enum { mask = (1u << nbits) - 1u };
 
+public:
     template <typename T2>
     RegBit& operator=(T2 val) {
         data = (data & ~(mask << bitno)) | (val & mask) << bitno; 
